4c410025bcf2775a3cf8e0ec44a92f8076fe37e1
dedarob <euclidesroberto16@gmail.com>
2025-04-16 16:30:19 -0300

    primeiro commit

---
+++ src/test/java/com/xanghay/casamarmorista/CasamarmoristaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xanghay.casamarmorista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasamarmoristaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
